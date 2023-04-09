6535f9d07323c13d45145bc5bb15077535a5037c
Jose Guglielmi <114435036+Jose-Guglielmi@users.noreply.github.com>
2023-04-09 20:27:11 -0300

    978954635252453453

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+//obtengo el boton de hamburguesa para el menu responsive
+const btn = document.querySelector(".hamburguesa");
+
+//le agrego un evento click para que al hacer click me verifique si tiene la clase open, si la tiene se la remuevo y si no la tiene se la agrego
+btn.addEventListener("click", function () {
+  //si el boton tiene la clase open
+  if (btn.classList.contains("open")) {
+    //le remuevo la clase open
+    btn.classList.remove("open");
+  } else {
+    //si no la tiene le agrego la clase open
+    btn.classList.add("open");
+  }
+});
+
+//obtener el boton flotante para el scroll
+const btnFlotante = document.querySelector(".btn-flotante");
+
+//agrego un evento scroll para que al hacer scroll me verifique si la posicion en Y es mayor a 70px, si es mayor a 70px le remuevo la clase ocultar y si no le agrego la clase ocultar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 70) {
+    //si la posicion en Y es mayor a 70px le remuevo la clase ocultar
+    btnFlotante.classList.remove("ocultar");
+  } else {
+    //si no, le agrego la clase ocultar
+    btnFlotante.classList.add("ocultar");
+  }
+});
